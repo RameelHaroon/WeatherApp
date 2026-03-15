38046ca4eb64593579292521b72a04b2241368f8
Rameel Haroon <RameelHaroonOff@gmail.com>
2026-03-15 14:24:16 +0500

    Project init

---
+++ WeatherApp/src/main/java/com/example/WeatherApp/WeatherAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.WheatherApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheatherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheatherAppApplication.class, args);
+	}
+
+}
